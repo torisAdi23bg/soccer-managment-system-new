f7396b372f7d4fa3fd629b9eab8687ac3ea0edf9
torisAdi23bg <toris@post.bgu.ac.il>
2022-05-13 11:34:25 +0300

    DOMAIN - UC IS READY

---
+++ src/Domain/Event.java
@@ -0,0 +1,14 @@
+package Domain;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Event {
+
+    public String date;
+    public Time time;
+    public int minue;
+    public EventType type;
+    public String description;
+
+}
---
+++ src/Domain/EventType.java
@@ -0,0 +1,6 @@
+package Domain;
+
+public enum EventType {
+    Goal, Offside, Foul, RedCard, YellowCard, Injury, Substitute
+
+}
