0745e7510491bbcd7ec5278c3142183a889d613b
hmoshayof <harel.moshayof@intel.com>
2022-05-16 18:37:29 +0300

    backup

--- src/Domain/Logger.java
+++ src/Domain/Logger.java
@@ -3,24 +3,73 @@ package Domain;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
-    private final String logFile = "log.txt";
-    private PrintWriter writer;
+    private final String logFile = "src/DataAccess/Log";
+    private  FileWriter writer;
+
+    private static Logger logger;
+
     private Logger() {
         try {
-            FileWriter fw = new FileWriter(logFile);
-            writer = new PrintWriter(fw, true);
+            writer = new FileWriter(logFile,true);
         } catch (IOException e) {}
     }
-    public void logLogin (String username, String password,String message) {
-        writer.println("USER LOGGED IN: USERNAME:"+username+"|PASSWORD:"+password+"|MESSAGE:"+message);
-    }
-    public void logAssignGames(String leagueID, String seasonID, boolean policy){
-        writer.println("USER ASSIGNED GAMES IN: LEAGUE:"+leagueID+"|SEASON:"+seasonID+"|POLICY:"+policy);
-    }
 
 
+    public static Logger getInstance(){
+
+        if (logger==null){
+            return  new Logger();
+        }
+        return logger;
+    }
+    public   void logLogin (String username, String password,String message) {
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            writer.write("USER LOGGED IN: USERNAME: "+username+" | PASSWORD: "+password+" | MESSAGE: "+message+" TIME: "+dtf.format(now)+"\n");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public  void logAssignGames(String leagueID, String seasonID, boolean policy,String message){
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            writer.write("USER ASSIGNED GAMES IN: LEAGUE:"+leagueID+"|SEASON:"+seasonID+"|POLICY:"+policy+" | MESSAGE:"+message+" TIME: "+dtf.format(now)+"\n");
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public  void logAssignReferee(String leagueID,String seasonID, String refereeUsername,String message){
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            writer.write("USER ASSIGNED REFEREE IN: LEAGUE:"+leagueID+"|SEASON:"+seasonID+"|REFEREE:"+refereeUsername+" |MESSAGE"+message+" TIME: "+dtf.format(now)+"\n");
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public  void lofLogout(String username){
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            writer.write("USER LOGGED OUT: USER: "+username+" TIME: "+dtf.format(now)+"\n");
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
--- src/Service/JavaApplication.java
+++ src/Service/JavaApplication.java
@@ -6,14 +6,20 @@ public class JavaApplication {
     public Controller domainController = Controller.getInstance();
 
     public String assignReferee(String leagueID,String seasonID, String refereeUsername) {
-        return domainController.assignReferee(leagueID,seasonID,refereeUsername);
+        String res= domainController.assignReferee(leagueID,seasonID,refereeUsername);
+        Logger.getInstance().logAssignReferee(leagueID, seasonID, refereeUsername,res);
+        return res;
     }
 
     public String assignGames(String leagueID, String seasonID, boolean policy){
-        return domainController.assignGames(leagueID, seasonID, policy);
+        String res=domainController.assignGames(leagueID, seasonID, policy);
+        Logger.getInstance().logAssignGames(leagueID, seasonID, policy,res);
+        return res;
     }
 
     public String login(String username,String password){
-        return domainController.login(username,password);
+        String res= domainController.login(username,password);
+        Logger.getInstance().logLogin(username, password,res);
+        return res;
     }
 }
