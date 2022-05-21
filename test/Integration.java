7bfc114a399fe581c0d181a2cad19d7c83ad6a93
perrys21 <perrys@post.bgu.ac.il>
2022-05-21 13:59:02 +0300

    perry - UNIT and integration

--- test/Integration.java
+++ test/Integration.java
@@ -12,86 +12,226 @@ public class Integration {
     JavaApplication app = new JavaApplication();
     Dao db=Dao.getInstance();
 
+    @Test
+    public void Int1_no_login_assign_policyTrue(){
+        //        no login done, assignGames shouldn't work
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
+    }
 
     @Test
-    public void user_is_logged_out_and_trying_to_assign_referee(){
+    public void Int2_no_login_assign_policyFalse(){
+        //        no login done, assignGames shouldn't work
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
+    }
+
+    @Test
+    public void Int3_Notexist_login_assign_policyTrue(){
+        //username not exist, assignGames shouldn't work
+        app.login("notexist", "1234");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int4_Notexist_login_assign_policyFalse(){
+        //username not exist, assignGames shouldn't work
+        app.login("notexist", "1234");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int5_login_as_regularSub_unsuccessful_assign_policyTrue(){
+        //login as regular subscriber unsuccessful so assignGames shouldn't work
+        app.login("1", "1234");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int6_login_as_regularSub_unsuccessful_assign_policyFalse(){
+        //login as regular subscriber unsuccessful so assignGames shouldn't work
+        app.login("1", "1234");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int7_login_as_regularSub_assign_policyTrue(){
+        //login as regular subscriber successful so assignGames shouldn't work
+        app.login("1", "1");
+        assertEquals("The user that is currently logged in is not an Association Representive",app.assignGames(legueId,seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int8_login_as_regularSub_assign_policyFalse(){
+        //login as regular subscriber successful so assignGames shouldn't work
+        app.login("1", "1");
+        assertEquals("The user that is currently logged in is not an Association Representive",app.assignGames(legueId,seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int9_login_as_AR_unsuccessful_assign_policyTrue(){
+        //login as AR unsuccessful so assignGames shouldn't work
+        app.login("2", "wrongPass");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int10_login_as_AR_unsuccessful_assign_policyFalse(){
+        //login as AR unsuccessful so assignGames shouldn't work
+        app.login("2", "wrongPass");
+        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int11_login_as_AR_assign_policyTrue_nullSeason(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(null,seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int12_login_as_AR_assign_policyFalse_nullSeason(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(null,seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int13_login_as_AR_assign_policyTrue_nullLeague(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(legueId,null,true));
+        app.logout();
+    }
+    @Test
+    public void Int14_login_as_AR_assign_policyFalse_nullLeague(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(legueId,null,false));
+        app.logout();
+    }
+    @Test
+    public void Int15_login_as_AR_assign_policyTrue_notExist_Season(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames("notexist",seasonID,true));
+        app.logout();
+    }
+    @Test
+    public void Int16_login_as_AR_assign_policyFalse_notExist_Season(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames("notexist",seasonID,false));
+        app.logout();
+    }
+    @Test
+    public void Int17_login_as_AR_assign_policyTrue_notExist_League(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(legueId,"notexist",true));
+        app.logout();
+    }
+    @Test
+    public void Int18_login_as_AR_assign_policyFalse_notExist_League(){
+        //login as AR successful. assignGames wont work because of null inputs
+        app.login("2", "2");
+        assertEquals("Enter valid details",app.assignGames(legueId,"notexist",false));
+        app.logout();
+    }
+    @Test
+    public void Int19_login_as_AR_assign_policyTrue(){
+        //login as AR successful so assignGames should work
+        app.login("2", "2");
+        assertNotEquals("",app.assignGames(legueId,seasonID,true));
+        db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
+        app.logout();
+    }
+    @Test
+    public void Int20_login_as_AR_assign_policyFalse(){
+        //login as AR successful so assignGames should work
+        app.login("2", "2");
+        assertNotEquals("",app.assignGames(legueId,seasonID,false));
+        db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
+        app.logout();
+    }
+
+    @Test
+    public void Int21_user_is_logged_out_and_trying_to_assign_referee(){
         app.logout();
         assertEquals("No logged-in association representative",app.assignReferee(legueId,seasonID,refereeUsername));
     }
     @Test
-    public void username_does_not_exist_but_trying_to_assign_referee(){
+    public void Int22_username_does_not_exist_but_trying_to_assign_referee(){
         //username not exist, assignReferee shouldn't work
         app.login("notexist", "1234");
         assertEquals("No logged-in association representative",app.assignReferee(legueId,seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_is_unsuccessful_and_trying_to_assign_referee(){
+    public void Int23_login_is_unsuccessful_and_trying_to_assign_referee(){
         //login as regular subscriber unsuccessful so assignReferee shouldn't work
         app.login("1", "1234");
         assertEquals("No logged-in association representative",app.assignReferee(legueId,seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_non_AR_subscriber_and_trying_to_assign_referee(){
+    public void Int24_login_as_non_AR_subscriber_and_trying_to_assign_referee(){
         //login as regular subscriber successful so assignReferee shouldn't work
         app.login("1", "1");
         assertEquals("No logged-in association representative",app.assignReferee(legueId,seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_is_unsuccessfully_and_trying_to_assign_referee(){
+    public void Int25_login_as_AR_is_unsuccessfully_and_trying_to_assign_referee(){
         //login as AR unsuccessful so assignReferee shouldn't work
         app.login("2", "wrongPass");
         assertEquals("No logged-in association representative",app.assignReferee(legueId,seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_league_is_empty_string(){
+    public void Int26_login_as_AR_and_trying_to_assign_referee_when_league_is_empty_string(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee("",seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_season_is_empty_string(){
+    public void Int27_login_as_AR_and_trying_to_assign_referee_when_season_is_empty_string(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee(legueId,"",refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_referee_is_empty_string(){
+    public void Int28_login_as_AR_and_trying_to_assign_referee_when_referee_is_empty_string(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee(legueId,seasonID,""));
         app.logout();
     }
 
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_league_is_not_exist(){
+    public void Int29_login_as_AR_and_trying_to_assign_referee_when_league_is_not_exist(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee("notexist",seasonID,refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_season_is_not_exist(){
+    public void Int30_login_as_AR_and_trying_to_assign_referee_when_season_is_not_exist(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee(legueId,"notexist",refereeUsername));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_when_referee_is_not_exist(){
+    public void Int31_login_as_AR_and_trying_to_assign_referee_when_referee_is_not_exist(){
         app.login("2", "2");
         assertEquals("Enter valid details",app.assignReferee(legueId,seasonID,"notexist"));
         app.logout();
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_referee_successfully(){
+    public void Int32_login_as_AR_and_trying_to_assign_referee_successfully(){
         app.login("2", "2");
         assertEquals("true",app.assignReferee(legueId,seasonID,refereeUsername));
         app.logout();
         db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
     }
     @Test
-    public void login_as_AR_and_trying_to_assign_same_referee_twice(){
+    public void Int33_login_as_AR_and_trying_to_assign_same_referee_twice(){
 //        login as AR successful. assignment done twice
         app.login("2", "2");
         app.assignReferee(legueId,seasonID,refereeUsername);
@@ -99,53 +239,4 @@ public class Integration {
         app.logout();
         db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
     }
-
-
-    @Test
-    public void login_and_assignGames(){
-//        no login done, assignGames shouldn't work
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
-
-        //username not exist, assignGames shouldn't work
-        app.login("notexist", "1234");
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
-        app.logout();
-
-        //login as regular subscriber unsuccessful so assignGames shouldn't work
-        app.login("1", "1234");
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
-        app.logout();
-
-        //login as regular subscriber successful so assignGames shouldn't work
-        app.login("1", "1");
-        assertEquals("The user that is currently logged in is not an Association Representive",app.assignGames(legueId,seasonID,true));
-        assertEquals("The user that is currently logged in is not an Association Representive",app.assignGames(legueId,seasonID,false));
-        app.logout();
-
-        //login as AR unsuccessful so assignGames shouldn't work
-        app.login("2", "wrongPass");
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,true));
-        assertEquals("No logged-in association representative",app.assignGames(legueId,seasonID,false));
-        app.logout();
-
-        //login as AR successful. assignGames wont work because of null inputs
-        app.login("2", "2");
-        assertEquals("Enter valid details",app.assignGames("",seasonID,true));
-        assertEquals("Enter valid details",app.assignGames(legueId,"",false));
-        app.logout();
-
-        //login as AR successful so assignGames should work
-        app.login("2", "2");
-        assertNotEquals("",app.assignGames(legueId,seasonID,true));
-        db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
-        assertNotEquals("",app.assignGames(legueId,seasonID,false));
-        app.logout();
-
-        db.deleteLeagueSeasonReferee(legueId,seasonID,refereeUsername);
-    }
-
-
 }
--- test/Unit.java
+++ test/Unit.java
@@ -3,8 +3,10 @@ import Domain.*;
 import org.junit.Test;
 
 import java.util.LinkedList;
+import java.util.function.ToDoubleBiFunction;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Unit {
     Referee ref1 = new Referee("ref1", "a 10 years experience","qual");
@@ -18,60 +20,208 @@ public class Unit {
     AssociationRepresentive AR = new AssociationRepresentive("2", "2");
 
     @Test
-    public void subscriber_login_test_failure() {
-        Subscriber sub1 = new Subscriber("user1", "password1");
-        assertEquals(false, sub1.login("password2"));
-    }
-
-    @Test
-    public void subscriber_login_test_success() {
-        Subscriber sub1 = new Subscriber("user1", "password1");
-        assertEquals(true, sub1.login("password1"));
-    }
-
-    @Test
-    public void Test1_AssignmentExist_wrong_inputs(){
+    public void Unit1_AssignmentExist_wrong_inputs(){
         ref1.assignments.add(new Pair(season1,league1));
         assertEquals(false, ref1.isAssignmentExists(season2,league2));
         ref1.assignments.remove(0);
     }
 
     @Test
-    public void Test2_AssignmentExist_wrong_inputs(){
+    public void Unit2_AssignmentExist_wrong_inputs(){
         ref1.assignments.add(new Pair(season1,league1));
         assertEquals(false, ref1.isAssignmentExists(season1,league2));
         ref1.assignments.remove(0);
     }
 
     @Test
-    public void Test3_AssignmentExist_wrong_inputs(){
+    public void Unit3_AssignmentExist_wrong_inputs(){
         ref1.assignments.add(new Pair(season1,league1));
         assertEquals(false, ref1.isAssignmentExists(season2,league1));
         ref1.assignments.remove(0);
     }
 
     @Test
-    public void Test4_AssignmentExist_correct_inputs(){
+    public void Unit4_AssignmentExist_correct_inputs(){
         ref1.assignments.add(new Pair(season1,league1));
         assertEquals(true, ref1.isAssignmentExists(season1,league1));
         ref1.assignments.get(0);
     }
 
     @Test
-    public void Test5_assignGames_policyTrue(){
+    public void Unit5_assignGames_policyTrue_even(){
         LinkedList<Game> result = AR.assignGames(league1, season1, true);
+        //all teams-BARCELONA,HAPOEL,MACCABI REAL
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+        assertEquals(result.get(0), expectedRes1.get(0));
     }
 
     @Test
-    public void Test6_assignGames_policyTrue_stub(){
+    public void Unit6_assignGames_policyTrue_odd(){
+        LinkedList<Game> result = AR.assignGames(league1, season2, true);
+        //all teams-BARCELONA,HAPOEL,MACCABI REAL
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+
+        assertEquals(result.get(0), expectedRes1.get(0));
+    }
+
+    private LinkedList<Game> true_stub(LinkedList<Team> teams){
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+        return expectedRes1;
+    }
+
+    private LinkedList<Game> assignGames_with_stub_policy1(League league, Season season, boolean policy){
+        Dao db= Dao.getInstance();
+        LinkedList<Team> allGroupsInSeasonLeague = db.getAllTeams(league.id,season.id);
+        LinkedList<Game> newGamesToSave;
+
+        if (policy==true){
+            //every couple have one game. so every team plays at most one game .the field will be for the team lexicographically first.
+            // If odd number - one team not playing.
+            newGamesToSave=true_stub(allGroupsInSeasonLeague);
+        }
+        else
+        {
+            //every team plays two games against every other team.
+            // the field will be one time for the first group, and another time for the second. So each team plays 2*(n-1) games
+            // (when n is number of groups).
+            newGamesToSave=null;
+        }
+        return newGamesToSave;
+    }
+
+    @Test
+    public void Unit7_assignGames_policyTrue_stub(){
         LinkedList<Game> result =  AR.assignGames(league1, season1, true);
-    }
-    @Test
-    public void Test7_assignGames_policyFalse(){
-        LinkedList<Game> result = AR.assignGames(league1, season1, false);
+        LinkedList<Game> stubed_results = this.assignGames_with_stub_policy1(league1, season1, true);
+        assertEquals(result, stubed_results);
     }
 
     @Test
-    public void Test8_assignGames_policyFalse_stub(){
-        LinkedList<Game> result = AR.assignGames(league1, season1, true);
+    public void Unit8_assignGames_policyFalse(){
+        LinkedList<Game> result = AR.assignGames(league1, season2, false);
+
+        //all teams-BARCELONA,HAPOEL,MACCABI REAL
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+        Team hapoel = new Team("HAPOEL", "H",true,true);
+
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, barcelona));
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, hapoel));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, hapoel));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, maccabi));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, barcelona));
+
+        boolean check_if_equal = true;
+        for (int i=0;i<result.size();i++){
+            check_if_equal = check_if_equal && result.get(i).equals(expectedRes1.get(i));
+        }
+        assertTrue(check_if_equal);
     }
+
+    private LinkedList<Game> false_stub(LinkedList<Team> teams){
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+        Team hapoel = new Team("HAPOEL", "H",true,true);
+
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, barcelona));
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, hapoel));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, hapoel));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, maccabi));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, barcelona));
+        return expectedRes1;
+    }
+
+    private LinkedList<Game> assignGames_with_stub_policy2(League league, Season season, boolean policy){
+        Dao db= Dao.getInstance();
+        LinkedList<Team> allGroupsInSeasonLeague = db.getAllTeams(league.id,season.id);
+        LinkedList<Game> newGamesToSave;
+
+        if (policy==true){
+            //every couple have one game. so every team plays at most one game .the field will be for the team lexicographically first.
+            // If odd number - one team not playing.
+            newGamesToSave=null;
+        }
+        else
+        {
+            //every team plays two games against every other team.
+            // the field will be one time for the first group, and another time for the second. So each team plays 2*(n-1) games
+            // (when n is number of groups).
+            newGamesToSave=false_stub(allGroupsInSeasonLeague);
+        }
+        return newGamesToSave;
+    }
+
+    @Test
+    public void Unit9_assignGames_policyFalse_stub(){
+        LinkedList<Game> result = AR.assignGames(league1, season2, false);
+        LinkedList<Game> stubed_result = this.assignGames_with_stub_policy2(league1, season2, false);
+        assertEquals(result, stubed_result);
+    }
+
+    @Test
+    public void Unit10_subscriber_login_test_failure() {
+        Subscriber sub1 = new Subscriber("user1", "password1");
+        assertEquals(false, sub1.login("password2"));
+    }
+
+    @Test
+    public void Unit11_subscriber_login_test_success() {
+        Subscriber sub1 = new Subscriber("user1", "password1");
+        assertEquals(true, sub1.login("password1"));
+    }
+
+    //TODO talk with harel
+
+    /*@Test
+    public void Test_assignpolicy1_even(){
+    }
+
+    @Test
+    public void Test_assignpolicy1_odd(){
+    }
+
+    @Test
+    public void Test_assignpolicy2(){
+        LinkedList<Team> Teams = new LinkedList<>();
+        Team maccabi = new Team("MACCABI", "M",true,true);
+        Team barcelona = new Team("BARCELONA", "B",true,true);
+        Team hapoel = new Team("HAPOEL", "H",true,true);
+
+        Teams.add(maccabi);
+        Teams.add(barcelona);
+        Teams.add(hapoel);
+
+        LinkedList<Game> result = AR.applypolicy2(Teams);
+
+
+        LinkedList<Game> expectedRes1 = new LinkedList<>();
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, barcelona));
+        expectedRes1.add(new Game(maccabi.homeField, maccabi, hapoel));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, maccabi));
+        expectedRes1.add(new Game(barcelona.homeField, barcelona, hapoel));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, maccabi));
+        expectedRes1.add(new Game(hapoel.homeField, hapoel, barcelona));
+
+        boolean check_if_equal = true;
+        for (int i=0;i<result.size();i++){
+            check_if_equal = check_if_equal && result.get(i).equals(expectedRes1.get(i));
+        }
+        assertTrue(check_if_equal);
+
+
+    }*/
+}
